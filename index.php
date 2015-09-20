870c60310ec75ae095cdb90cc4df0cb82d048474
Alexandre Plennevaux <pixeline@users.noreply.github.com>
2015-09-20 22:52:27 +0200

    Utilisation de tes variables locales partout.
    - A partir du moment où tu décides de stocker les variables de $_GET dans des variables locales, autant le faire partout.
    - prend l'habitude de mettre au maximum tout le PHP AVANT le html, notamment ici tes fonctions, histoire de commencer à séparer la partie "calculs et traitements" de la partie "affichage".

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+
+//Création des variables boléènnes et récupération des données entrées dans le formulaire
+$combi1 = $_GET['combi1'];
+$combi2 = $_GET['combi2'];
+$combi3 = $_GET['combi3'];
+$combi4 = $_GET['combi4'];
+$combi5 = $_GET['combi5'];
+$combi6 = $_GET['combi6'];
+
+//Mes fonctions 
+//Fonction qui approuve le pret de la voiture
+function jePrete() {
+	echo "<div class='reponse'><img class='bmw' src='bmw.png' alt='Image illustrant la BMW' /><p>Combinaison magique trouvée! Voici la voiture !</p></div>" ;
+}
+
+//Fontion qui approuve le pret de la voiture avec condition
+function jeDisNon() {
+	echo "<p class='reponse'>Essaies encore, ton père ne te pretera pas sa voiture si facilement !</p>" ;	
+}	
+			
+//Fonction qui désapprouve le pret de la voiture.
+function jeConditionne() {
+	echo "<p class='reponse'>Ok pour cette fois, mais n'oublies pas de me ramener quelque chose alors !</p>" ;				
+}
+?>
 <!doctype html>
 <html lang="fr">
 
@@ -20,48 +46,48 @@
 				<fieldset>
 					<legend>Vas-tu boire de l'acool à cette soirée ? </legend>
 						<label for="question1">Oui</label>
-						<input id="question1" type="radio" name="combi1" value="true" checked <?php if( $_GET['combi1']=='true'){ echo 'checked="checked"';} ?> required>
+						<input id="question1" type="radio" name="combi1" value="true" checked <?php if( $combi1=='true'){ echo 'checked="checked"';} ?> required>
 						<label for="question12">Non</label>
-						<input id="question12" type="radio" name="combi1" value="false" <?php if( $_GET['combi1']=='false'){ echo 'checked="checked"';} ?> required>
+						<input id="question12" type="radio" name="combi1" value="false" <?php if( $combi1=='false'){ echo 'checked="checked"';} ?> required>
 				</fieldset>
 
 				<fieldset>
 					<legend>Connais-tu Bob ?</legend>
 						<label for="question2">Oui</label>
-						<input id="question2"type="radio" name="combi2" value="true" checked <?php if( $_GET['combi2']=='true'){ echo 'checked="checked"';} ?> required>
+						<input id="question2"type="radio" name="combi2" value="true" checked <?php if( $combi2=='true'){ echo 'checked="checked"';} ?> required>
 						<label for="question22">Non</label>
-						<input id="question22" type="radio" name="combi2" value="false" <?php if( $_GET['combi2']=='false'){ echo 'checked="checked"';} ?> required>
+						<input id="question22" type="radio" name="combi2" value="false" <?php if( $combi2=='false'){ echo 'checked="checked"';} ?> required>
 				</fieldset>
 					
 				<fieldset>	
 					<legend>Est ce qu'il neige ?</legend>
 						<label for="question3">Oui</label>
-						<input id="question3" type="radio" name="combi3" value="true" checked <?php if( $_GET['combi3']=='true'){ echo 'checked="checked"';} ?> required>
+						<input id="question3" type="radio" name="combi3" value="true" checked <?php if( $combi3=='true'){ echo 'checked="checked"';} ?> required>
 						<label for="question32">Non</label>
-						<input id="question32" type="radio" name="combi3" value="false" <?php if( $_GET['combi3']=='false'){ echo 'checked="checked"';} ?> required>
+						<input id="question32" type="radio" name="combi3" value="false" <?php if( $combi3=='false'){ echo 'checked="checked"';} ?> required>
 				</fieldset>
 
 				<fieldset>
 					<legend>As tu de l'argent pour mettre de l'essence? </legend>
 						<label for="question4">Oui</label>
-						<input id="question4" type="radio" name="combi4" value="true" checked <?php if( $_GET['combi4']=='true'){ echo 'checked="checked"';} ?> required>
+						<input id="question4" type="radio" name="combi4" value="true" checked <?php if( $combi4=='true'){ echo 'checked="checked"';} ?> required>
 						<label for="question42">Non</label>
-						<input id="question42" type="radio" name="combi4" value="false" <?php if( $_GET['combi4']=='false'){ echo 'checked="checked"';} ?> required>
+						<input id="question42" type="radio" name="combi4" value="false" <?php if( $combi4=='false'){ echo 'checked="checked"';} ?> required>
 				</fieldset>
 
 				<fieldset>
 					<legend>Comptes tu encore aller jusque maastricht?</legend>
 						<label for="question5">Oui</label>
-						<input id="question5" type="radio" name="combi5" value="true" checked <?php if( $_GET['combi5']=='true'){ echo 'checked="checked"';} ?> required>
+						<input id="question5" type="radio" name="combi5" value="true" checked <?php if( $combi5=='true'){ echo 'checked="checked"';} ?> required>
 						<label for="question52">Non</label>
-						<input id="question52" type="radio" name="combi5" value="false" <?php if( $_GET['combi5']=='false'){ echo 'checked="checked"';} ?> required>
+						<input id="question52" type="radio" name="combi5" value="false" <?php if( $combi5=='false'){ echo 'checked="checked"';} ?> required>
 				</fieldset>
 				<fieldset>
 					<legend>Vas-tu ramener tes copains qui boivent toujours trop ?</legend>
 						<label for="question6">Oui</label>
-						<input id="question6" type="radio" name="combi6" value="true" checked <?php if( $_GET['combi6']=='true'){ echo 'checked="checked"';} ?> required>
+						<input id="question6" type="radio" name="combi6" value="true" checked <?php if( $combi6=='true'){ echo 'checked="checked"';} ?> required>
 						<label for="question61">Non</label>
-						<input id="question61" type="radio" name="combi6" value="false" <?php if( $_GET['combi6']=='false'){ echo 'checked="checked"';} ?>required>
+						<input id="question61" type="radio" name="combi6" value="false" <?php if( $combi6=='false'){ echo 'checked="checked"';} ?>required>
 				</fieldset>
 
 
@@ -73,29 +99,9 @@
 		<?php
 			
 
-			//Mes fonctions 
-				//Fonction qui approuve le pret de la voiture
-				function jePrete() {
-					echo "<div class='reponse'><img class='bmw' src='bmw.png' alt='Image illustrant la BMW' /><p>Combinaison magique trouvée! Voici la voiture !</p></div>" ;
-				}
+		
 
-				//Fontion qui approuve le pret de la voiture avec condition
-				function jeDisNon() {
-					echo "<p class='reponse'>Essaies encore, ton père ne te pretera pas sa voiture si facilement !</p>" ;	
-				}	
-			
-				//Fonction qui désapprouve le pret de la voiture.
-				function jeConditionne() {
-					echo "<p class='reponse'>Ok pour cette fois, mais n'oublies pas de me ramener quelque chose alors !</p>" ;				
-				}
 
-			//Création des variables boléènnes et récupération des données entrées dans le formulaire
-			$combi1 = $_GET['combi1'];
-			$combi2 = $_GET['combi2'];
-			$combi3 = $_GET['combi3'];
-			$combi4 = $_GET['combi4'];
-			$combi5 = $_GET['combi5'];
-			$combi6 = $_GET['combi6'];
 
 			//IF pour lancer les fonction qui donnent le vedict et vérification des variables boléènnes.
 			if ($combi1 == "false" && $combi2 == "true" && $combi3 == "false"  && $combi4 == "true" && $combi5 == "false"  && $combi6 == "false") {
